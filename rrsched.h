850e740a7628c87565070373f78ca1d9e5fb99e9
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2020-04-12 00:07:26 +0200

    Implement schedule() and waitingTime() in RR Scheduler

--- rrsched.h
+++ rrsched.h
@@ -1,11 +1,14 @@
 #ifndef RRSCHED_H
 #define RRSCHED_H
 #include "scheduler.h"
+#include <QQueue>
+#include <QtAlgorithms>
 
 class RRSched : public Scheduler
 {
 private:
     float quantumTime;
+    static bool cmp(const SysProcess &p1 , const SysProcess &p2);
 public:
     RRSched();
     void schedule();
